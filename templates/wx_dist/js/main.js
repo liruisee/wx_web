8f9204e1b583b288034cdd6805a6ad70472f418b
王川川 <devilfrank_isy@163.com>
2018-05-12 18:28:01 +0800

    wrarse

--- templates/wx_dist/js/main.js
+++ templates/wx_dist/js/main.js
@@ -1,15 +1,10 @@
 document.getElementsByTagName("html")[0].style.fontSize=document.documentElement.clientWidth/16+"px";//改变窗口的时候重新计算大小
 window.onresize = function(){document.getElementsByTagName("html")[0].style.fontSize=document.documentElement.clientWidth/16+"px";}
 
-var tea_type = {
-    's':'上忍',
-    'a':'老师',
-    'r':'演员'
-}
 
-var child = {
+var typeListchild = {
     props:['img_url','teacher_id','teacher_name','teacher_record','teacher_type','video_url'],
-    template:'<li @click="jump(teacherId)"><img :src=imgUrl alt="" class="box_list_img"><span class="teachername">{{ teacherName }}</span></li>',
+    template:'<li @click="jump(teacherId)"><img v-bind:src=imgsrc alt="" class="box_list_img"><span class="teachername">{{ teacherName }}</span></li>',
     data:function(){
         return {
             'imgUrl':this.img_url,
@@ -17,7 +12,8 @@ var child = {
             'teacherName':this.teacher_name,
             'teacherRecord':this.teacher_record,
             'teacherType':this.teacher_type,
-            'videoUrl':this.video_url
+            'videoUrl':this.video_url,
+            'imgsrc':'images/'+this.img_url
         }
     },
     methods:{
@@ -25,4 +21,20 @@ var child = {
             console.log(e);
         }
     }
+}
+
+var teacherlistchild = {
+    props:['img_url','teacher_id','teacher_name','teacher_record','teacher_type','video_url'],
+    template:'<li><div class="teacher_list_img"><img v-bind:src=imgsrc alt=""></div><div class="teacher_list_msg"><p class="list_msg_name">{{teacherName}}</p><p class="list_msg_type"><span>类别：</span>{{teacherType}}</p><p class="list_msg_brief"><span>简介：</span>{{teacherRecord}}</p></div></li>',
+    data:function(){
+        return {
+            'imgUrl':this.img_url,
+            'teacherId':this.teacher_id,
+            'teacherName':this.teacher_name,
+            'teacherRecord':this.teacher_record,
+            'teacherType':this.teacher_type,
+            'videoUrl':this.video_url,
+            'imgsrc':'images/'+this.img_url
+        }
+    },
 }
